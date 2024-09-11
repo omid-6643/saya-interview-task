eccf8225451f37d0e7f32788e114c3ecca1a17e2
omid-6643 <o.beygi@gmail.com>
2024-09-11 19:04:04 +0330

    fix : fix deploy script

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
+import cors from "cors";
 import express from "express";
 import path from "path";
 import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
 import postRoutes from "./routes/post.route.js";
-import cors from 'cors'
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -16,10 +16,12 @@ app.options("*", cors());
 app.use("/api/v1/post", postRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/client/.next")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", ".next", "server", "app", "index.html")
+    );
   });
 }
 
