98a0dcf5d5977399abfca7380d6dd489a5ef0589
omid-6643 <o.beygi@gmail.com>
2024-09-11 20:17:52 +0330

    fix : change tsconfig

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,17 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-import path from "path";
-
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
